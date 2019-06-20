0a0f3ce367e62d629d520f00fad2830c76ce8ad8
SarthakJariwala <jariwala@uw.edu>
2019-06-19 17:06:30 -0700

    adding main app

---
+++ Microscope_app.py
@@ -0,0 +1,42 @@
+from ScopeFoundry import BaseMicroscopeApp
+
+class OceanOpticsApp(BaseMicroscopeApp):
+
+    # this is the name of the microscope that ScopeFoundry uses 
+    # when storing data
+    name = 'microscope'
+    
+    # You must define a setup function that adds all the 
+    #capablities of the microscope and sets default settings
+    def setup(self):
+    
+        #Add Hardware components
+        from HW_OceanOptics.OceanOptics_hardware import OceanOpticsHW
+        self.add_hardware(OceanOpticsHW(self))
+        from HW_PI_PiezoStage.PiezoStage_hardware import PiezoStageHW
+        self.add_hardware(PiezoStageHW(self))
+
+        #Add Measurement components
+        from HW_OceanOptics.OceanOptics_measurement import OceanOpticsMeasure
+        self.add_measurement(OceanOpticsMeasure(self))
+        #from PiezoStage_measurement import PiezoStageMeasure
+        #self.add_measurement(PiezoStageMeasure(self))
+        from HW_PI_PiezoStage.PiezoStage_measurement_liveImage import PiezoStageMeasureLive
+        self.add_measurement(PiezoStageMeasureLive)
+        # show ui
+        self.ui.show()
+        self.ui.activateWindow()
+
+    def on_close(self): #temp fix for properly closing the additional imageview window
+        BaseMicroscopeApp.on_close(self)
+        try:
+            liveupdate = self.measurements["oceanoptics_scan_liveupdate"]
+            liveupdate.imv.close()
+        except:
+            pass
+
+if __name__ == '__main__':
+    import sys
+    
+    app = OceanOpticsApp(sys.argv)
+    sys.exit(app.exec_())
