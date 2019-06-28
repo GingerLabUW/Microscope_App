d7f6a25f32c16cc8e9754e7ad1bd33012628f466
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-06-28 11:31:33 -0700

    added piezo control measurement

--- Microscope_app.py
+++ Microscope_app.py
@@ -33,6 +33,9 @@ class MicroscopeApp(BaseMicroscopeApp):
 
         from HW_PI_PiezoStage.PiezoStage_independent_movement import PiezoStageIndependentMovement
         self.add_measurement(PiezoStageIndependentMovement)
+
+        from HW_PI_PiezoStage.PiezoStage_control import PiezoStageControl
+        self.add_measurement(PiezoStageControl)
         # show ui
         self.ui.show()
         self.ui.activateWindow()
