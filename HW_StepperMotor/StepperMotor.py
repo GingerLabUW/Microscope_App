ed88186ba6809efa47cdb8fc9bb08af4ba6d70ff
Raj Giri <rgiri@uw.edu>
2019-08-09 16:02:51 -0700

    added steppermotor wrapper, debugged and completed sm_hardware

---
+++ HW_StepperMotor/StepperMotor.py
@@ -0,0 +1,61 @@
+import serial
+import sys
+
+class StepperMotor(serial.Serial):
+	def __init__(self, port, baudrate):
+		serial.Serial.__init__(self, port=port, baudrate=baudrate, stopbits=serial.STOPBITS_TWO, write_timeout=2., dsrdtr=True)
+
+	def readAndPrintBuffer(self):
+		print('\n' + self.readBuffer() + '\n')
+		
+
+	def readBuffer(self):
+		c=self.read().decode();
+		thisStr='';
+		while c is not '$':
+			thisStr += c
+			c=self.read().decode()
+		return thisStr
+
+	def set_port(self, new_port):
+		self.port = new_port
+
+	def get_port(self):
+		return self.port
+
+	def set_baudrate(self, new_baudrate):
+		self.baudrate = new_baudrate
+
+	def get_baudrate(self):
+		return self.baudrate
+
+	def get_position(self):
+		self.write(b"p")
+		position = self.readBuffer().split(",")
+		try:
+			position = [int(num) for num in position] #convert string to int
+		except:
+			pass
+		return position
+
+	def goto(self, input_array):
+		#Check input arguments
+		if len(input_array)<3:
+			motionType="a"
+		else:
+			motionType=input_array[2]
+
+		#Build string to send to controller
+		strToSend="g" + motionType
+		for arg in input_array[0:2]:
+			thisNumber=int(round(float(arg)*1000))
+			strToSend += str(thisNumber) + ","
+
+		strToSend=strToSend[:-1] + "$"
+
+		#Send string to OpenStage (initiates motion automatically)
+		self.write(strToSend.encode()) #encode() converts string to bytes
+
+		#Now block and wait for terminator
+		while self.read().decode() is not "$":
+			0
--- HW_StepperMotor/StepperMotor_hardware.py
+++ HW_StepperMotor/StepperMotor_hardware.py
@@ -1,55 +1,77 @@
 from ScopeFoundry import HardwareComponent
+from .StepperMotor import StepperMotor
 import serial
-
+import sys
 class StepperMotorHW(HardwareComponent):
 	
 	def setup(self):
 		# Define your hardware settings here.
 		# These settings will be displayed in the GUI and auto-saved with data files
-		self.name = 'steppermotor'
-		self.settings.New('port', dtype=str, choices=[("", ""), ("COM1", "COM1"), ("COM2", "COM2"), ("COM3", "COM3"), ("COM4", "COM4"),
-			("COM5", "COM5")], initial="")
-		self.settings.New('baudrate', dtype=float, initial=115200)
+		self.name = "steppermotor"
+		self.settings.New("port", dtype=str, choices=[("", ""), ("COM1", "COM1"), ("COM2", "COM2"), ("COM3", "COM3"), ("COM4", "COM4"),
+			("COM5", "COM5")], initial="COM5")
+		self.settings.New("baudrate", dtype=float, initial=115200)
+
+		self.settings.New("x_position", dtype=float, initial=0, unit="um")
+		self.settings.New("y_position", dtype=float, initial=0, unit="um")
+
+		self.settings.New("x_abs", dtype=float, initial=0, unit="um")
+		self.settings.New("y_abs", dtype=float, initial=0, unit="um")
+		self.add_operation("absolute_movement", self.abs_mov)
+		
+		self.settings.New("x_rel", dtype=float, initial=0, unit="um")
+		self.settings.New("y_rel", dtype=float, initial=0, unit="um")
+		self.add_operation("relative_movement", self.rel_mov)
+
+		self.settings.New("new_port", dtype=str, choices=[("COM1", "COM1"), ("COM2", "COM2"), ("COM3", "COM3"), ("COM4", "COM4"),
+			("COM5", "COM5")], initial="COM1")
+		self.add_operation("set_port", self.set_port)
+		self.settings.New("new_baudrate", dtype=float)
+		self.add_operation("set_baudrate", self.set_baudrate)
 
 	def connect(self):
 		# Open connection to the device:
-		port = self.settings['port']
-		baudrate = self.settings['baudrate']
-		self.stepper_motor = serial.Serial(port=port, baudrate=baudrate, stopbits=serial.STOPBITS_TWO, write_timeout=2., dsrdtr=True)
+		port = self.settings["port"]
+		baudrate = self.settings["baudrate"]
+		sm = self.stepper_motor = StepperMotor(port, baudrate)
 
 		#Connect settings to hardware:
 		LQ = self.settings.as_dict()
-		LQ["baudrate"].hardware_read_func = self.getBaudrate
-		LQ["baudrate"].hardware_set_func = self.setBaudrate
+		LQ["x_position"].hardware_read_func = lambda sm=sm: sm.get_position()[0]
+		LQ["y_position"].hardware_read_func = lambda sm=sm: sm.get_position()[1]
+		LQ["port"].hardware_read_func = lambda sm=sm: sm.get_port()
+		LQ["baudrate"].hardware_read_func = lambda sm=sm: sm.get_baudrate()
 
-		LQ["port"].hardware_read_func = self.getPort
-		LQ["port"].hardware_set_func = self.setPort
-	
-		#Take an initial sample of the data.
 		self.read_from_hardware()
-		
-	def getBaudrate(self):
-		return self.stepper_motor.baudrate
 
-	def setBaudrate(self):
+	def set_port(self):
+		self.stepper_motor.port = self.settings["new_port"]
+		self.settings.port.read_from_hardware()
+
+	def set_baudrate(self):
+		self.stepper_motor.baudrate = self.settings["new_baudrate"]
+		self.settings.baudrate.read_from_hardware()
+
+	def abs_mov(self):
 		if hasattr(self, "stepper_motor"):
-			self.stepper_motor.baudrate = self.settings["baudrate"]
-			self.read_from_hardware()
+			x_abs_pos = self.settings["x_abs"]
+			y_abs_pos = self.settings["y_abs"]
+			self.stepper_motor.goto([x_abs_pos, y_abs_pos])
+			self.settings.x_position.read_from_hardware()
+			self.settings.y_position.read_from_hardware()
 
-	def getPort(self):
-		return self.stepper_motor.port
-
-	def setPort(self):
-		try:
-			self.stepper_motor.port = self.settings["port"]
-			self.read_from_hardware()
-		except:
-			pass
-
+	def rel_mov(self):
+		if hasattr(self, "stepper_motor"):
+			x_rel_pos = self.settings["x_rel"]
+			y_rel_pos = self.settings["y_rel"]
+			self.stepper_motor.goto([x_rel_pos, y_rel_pos, "r"])
+			self.settings.x_position.read_from_hardware()
+			self.settings.y_position.read_from_hardware()
+	
 	def disconnect(self):
 		#Disconnect the device and remove connections from settings
 		self.settings.disconnect_all_from_hardware()
-		if hasattr(self, 'stepper_motor'):
+		if hasattr(self, "stepper_motor"):
 			self.stepper_motor.close()
 			del self.stepper_motor
 			self.stepper_motor = None
